0e54c0a7a7535057c81d70be69560d5cf7e407de
reThink23 <42834529+reThink23@users.noreply.github.com>
2023-06-12 22:47:48 +0200

    add Laufzeit & incomplete HeapSort

--- src/ueb9/HeapSort.java
+++ src/ueb9/HeapSort.java
@@ -8,6 +8,12 @@ import java.util.ArrayList;
 
 public class HeapSort {
 
+	private static boolean hasLeftChild( int i, int len ) { return (i <= len/2-1); }
+	private static boolean hasRightChild( int i, int len ) {return (i <= (len+1)/2-2);}
+	private static int left( int i, int len ) { if (hasLeftChild(i, len)) {return 2*i+1;} else throw new IndexOutOfBoundsException("i is out of bounds"); }
+	private static int right( int i, int len ) { if (hasRightChild(i, len)) {return 2*i+2;} else throw new IndexOutOfBoundsException("i is out of bounds"); }
+	private static int parent( int i ) { return i > 0 ? (i-1)/2 : -1; }
+	
 	public static void sort(int[] A) {
 		createHeap(A);
 
@@ -18,18 +24,20 @@ public class HeapSort {
 			heapify(A, last, 0);
 		}
 	}
-	
+
 	private static void heapify(int[] A, int k, int i) {
 		if (i < 0 || i >= k) return;
 		int len = A.length;
-		if (!hasLeftChild(i, len) && !hasRightChild(i, len)) return;
+		// if (!hasLeftChild(i, len) && !hasRightChild(i, len)) return;
+		if (!hasLeftChild(i, len)) return;
 		int child;
-		if (hasRightChild(i, len) && right(i, len) >= left(i, len)) { 
-			child = right(i, len);
+		if (hasLeftChild(i, len) && !hasRightChild(i, len)) { child = left(i,len); }
+		if (hasRightChild(i, len) && A[left(i, len)] > A[right(i, len)]) { 
+			child = left(i, len);
 		} else {
-			child = left(i, len); 
+			child = right(i, len); 
 		}
-		if (A[child] > A[i]) {
+		if (child != -1 && A[child] > A[i]) {
 			int tmp = A[child];
 			A[child] = A[i];
 			A[i] = tmp;
@@ -43,12 +51,6 @@ public class HeapSort {
 			heapify(A, len, i);
 		}
 	}
-
-	private static int left( int i, int len ) { return hasLeftChild(i, len) ? 2*i+1 : -1; }
-	private static int right( int i, int len ) { return hasRightChild(i, len) ? 2*i+2 : -1; }
-	private static int parent( int i ) { return i > 0 ? (i-1)/2 : -1; }
-	private static boolean hasLeftChild( int i, int len ) { return (i <= len/2-1); }
-	private static boolean hasRightChild( int i, int len ) {return (i <= (len+1)/2-2);}
 
 	public static String[] readLines(String filePath) throws IOException, FileNotFoundException {
 		ArrayList<String> stringList = new ArrayList<String>();
@@ -87,7 +89,7 @@ public class HeapSort {
 		long end = System.currentTimeMillis();
 		long diff = end - start;
 		System.out.println("Time: " + diff);
-		System.out.println("[" + A[0]+ ", ..., "+ A[A.length/2-1] +", ..., " + A[A.length-1]+"]");
+		// System.out.println("[" + A[0]+ ", ..., "+ A[A.length/2-1] +", ..., " + A[A.length-1]+"]");
 
 
 		// for (int a : A) {
