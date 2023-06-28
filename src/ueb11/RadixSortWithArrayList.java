7815ffbf6942306b5d48e729061109db13efb087
reThink23 <42834529+reThink23@users.noreply.github.com>
2023-06-28 08:33:08 +0200

    working code

--- src/ueb11/RadixSortWithArrayList.java
+++ src/ueb11/RadixSortWithArrayList.java
@@ -5,102 +5,47 @@ import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.stream.Collectors;
 
 public class RadixSortWithArrayList {
 	private int[] A;
+	private ArrayList<Integer> arr;
 	@SuppressWarnings("unchecked")
 	ArrayList<Integer>[] bucket = (ArrayList<Integer>[]) new ArrayList[2];
 	
+	{
+		bucket[0] = new ArrayList<Integer>();
+		bucket[1] = new ArrayList<Integer>();
+	}
+	
 	public RadixSortWithArrayList(int[] A) {
+		this.arr  = Arrays.stream( A ).boxed().collect( Collectors.toCollection(ArrayList::new) );
 		this.A = A;
 	}
 
 	public int[] sort() {
-		// int[] queue = new int[A.length];
-		// System.arraycopy(A, 0, queue, 0, A.length);
-		// int k = maxLength(A);
 		int k = 32;
-		// for (int r = k; r >= 1; r--) {
 		for (int r = 0; r < k; r++) {
 			clearBuckets();
-			while (A.length > 0) {
-				int num = pop();
+			while (arr.size() > 0) {
+				int num = arr.get(0);
+				arr.remove(0);
 				int idx = getBitAt(num, r);
 				bucket[idx].add(num);
-			}	
-			// A = flatten(bucket);
+			}
+			arr.clear();
+			arr.addAll(bucket[0]);
+			arr.addAll(bucket[1]);
 		} 
 		return A;
-		// int lenMax = maxLength(A);
-
-		// int[][] buckets = new int[lenMax][10];
-		// for (int i = 0; i < A.length; i++) {
-		// 	int len = getIntLength(A[i]);
-		// 	buckets[len] = add(buckets[len], A[i]);
-		// }
-		// A = flatten(buckets);
-
-
-		// int[][] buckets = new int[2][0];
-		// for (int i = 0; i < buckets.length; i++) {
-		// 	for (int j = 0; j < buckets[i].length; j++) {
-		// 		buckets[i][j] = j;
-		// 	}
-		// }
-		// for (int i = 0; i < A.length; i++) {
-		// 	int len = getIntLength(A[i]);
-		// 	buckets[A[i]] = add(buckets[A[i]], i);
-		// }
-		// A = flatten(buckets);
 	}
 
-	
-	private static int getIntLength(int a) {
-		return (a == 0) ? 1 : (int)(Math.log10(a)+1);
-	}
-
-	private static int maxLength(int[] A) {
-		int max = getIntLength(A[0]);
-		for (int i = 1; i < A.length; i++) {
-			int crtLen = getIntLength(A[i]);
-			if (crtLen > max) {
-				max = crtLen;
-			}
-		}
-		return max;
-	}
-
-
-
-	private int pop() {
-		if (A.length == 0) return -1;
-		int[] arrB = new int[A.length-1];
-		int first = A[0];
-		System.arraycopy(A, 1, arrB, 0, A.length-1);
-		A = arrB;
-		return first;
-	}
 
 	private void clearBuckets() {
 		for (int i = 0; i < bucket.length; i++) {
 			bucket[i].clear();
 		}
-	}
-
-	private static int[] concat(int[] arrA, int[] arrB) {
-		int[] arrRes = new int[arrA.length+arrB.length];
-		System.arraycopy(arrA, 0, arrRes, 0, arrA.length);
-		System.arraycopy(arrB, 0, arrRes, arrA.length, arrB.length);
-		return arrRes;
-	}
-
-	private static int[] flatten(int[][] arr) {
-		int[] arrRes = new int[arr[0].length];
-		System.arraycopy(arr[0], 0, arrRes, 0, arr[0].length);
-		for (int i = 1; i < arr.length; i++) {
-			arrRes = concat(arrRes, arr[i]);
-		}
-		return arrRes;
 	}
 
 	private static int getBitAt(int a, int i) {
@@ -133,7 +78,7 @@ public class RadixSortWithArrayList {
 
 	public static void main(String[] args) throws Exception {
 		if (args.length < 1) { 
-			System.out.println("Usage: java RadixSort <filename>"); 
+			System.out.println("Usage: java RadixSortWithArrayList <filename>"); 
 			System.exit(1);
 		}
 
